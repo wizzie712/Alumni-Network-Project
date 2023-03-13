629fdc76e738e0eb279c01e93eb76d8342c8dd69
Nikhil <78159183+wizzie712@users.noreply.github.com>
2023-03-13 15:33:53 +0530

    @media queries inserted

--- project/src/app/components/StudentLogin/login.component.ts
+++ project/src/app/components/StudentLogin/login.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component2.css']
 })
 export class LoginComponent implements OnInit{
   [x: string]: any;
